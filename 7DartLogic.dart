af1de3497be6d08751336d55d1dcb7ae800be1e1
ZippoAI <iamrealzippo@gmail.com>
2026-06-04 22:19:43 +0530

    dart logic done

---
+++ 7DartLogic.dart
@@ -0,0 +1,22 @@
+void main() {
+  // Logic -- if, If/Else, If Else If
+
+  //   var num = 3;
+
+  //   if (num == 5) {
+  //     print('number is 5');
+  //   } else {
+  //     print('Number is 3');
+  //   }
+
+  //  If Else If
+
+  var num = 3;
+  if (num == 5) {
+    print('Number is 5');
+  } else if (num == 3) {
+    print('the number is 3');
+  } else {
+    print('number is not 5 or 3');
+  }
+}
