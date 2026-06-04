e043c92b93f134e0c02a19e2d7f2e16859543402
ZippoAI <iamrealzippo@gmail.com>
2026-06-04 12:04:11 +0530

    dart list done

--- 4.DartLists.dart
+++ 4.DartLists.dart
@@ -1,1 +1,57 @@
 // Dart Lists
+
+void main() {
+  var myList = [1, 2, 3];
+  print(myList);
+
+  print(myList[0]);
+
+  // Change an item
+
+  myList[0] = 41;
+  print(myList);
+
+  // Create an Empty List
+
+  var emptyList = [];
+  print('Empty List:  $emptyList');
+
+  // Add To Empty List
+
+  emptyList = [2, 3, 4];
+  print('Empty List filled: $emptyList');
+
+  // Add more to the empty list
+  emptyList.add(55);
+  print(emptyList);
+
+  // Add multuple items to empty list
+  emptyList.addAll([66, 67, 68]);
+  print(emptyList);
+
+  // Insert at specific position (position, item)
+
+  var myList2 = [1, 2, 3];
+  myList2.insert(0, 900);
+  print(myList2);
+
+  // Insert Many
+  myList2.insertAll(4, [99, 98, 97]);
+  print(myList2);
+
+  // Mixed List
+
+  var mixedlist = [1, 2, 3, 'zippo', 'coder', 'remove'];
+  print(mixedlist);
+
+  // Remove item from list
+
+  mixedlist.remove(
+    'zippo',
+  ); // make sure zippo is one time on the list other wise we have to mention the specific location of the target
+  print(mixedlist);
+
+  // Remove from specific location
+  mixedlist.removeAt(4);
+  print(mixedlist);
+}
