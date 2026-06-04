bd3c7e24485496a32c7e8eb4f5a3b27d17e83198
ZippoAI <iamrealzippo@gmail.com>
2026-06-04 11:40:42 +0530

    dtypes done

---
+++ 3_DartDtypes.dart
@@ -0,0 +1,21 @@
+void main() {
+
+  // Strings
+  String firstName = 'Bulbul';
+  print('String: $firstName');
+
+  // Integers
+  int myNum = 5;
+  print('Int: $myNum');
+
+  //Doubles
+  double otherNum = 5.500;
+  print('Double: $otherNum');
+
+  // Booleans
+  bool myBoolT = true;
+  bool myBoolF = false;
+
+  print('Boolean True: $myBoolT \nBoolean False: $myBoolF');
+
+}
