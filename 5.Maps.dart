d988bfb9e324123a041a8ca59e7d9600f66ddf8c
ZippoAI <iamrealzippo@gmail.com>
2026-06-04 12:19:26 +0530

    starting maps

---
+++ 5.Maps.dart
@@ -0,0 +1,18 @@
+// Map == Dictionary in Python
+
+
+// Map! Key/Value Pairs
+
+// Show value
+
+// Show keys
+
+// Show lenght
+
+// Add Something
+
+// Add Many Things
+
+// Remove Something
+
+// Remove Everything
