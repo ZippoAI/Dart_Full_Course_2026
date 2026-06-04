9cbb8860fc0cfdb47858faee90dff25c3730ffb1
ZippoAI <iamrealzippo@gmail.com>
2026-06-04 16:08:33 +0530

    learning loop

---
+++ 6.DartLoops.dart
@@ -0,0 +1,20 @@
+void main() {
+  // For loop
+
+  var num = 5;
+
+  for (var i = num; i >= 1; i--) {
+    print(i);
+  }
+
+  print('-------------------');
+
+  for (var i = 0; i <=5; i++) {
+    print(i);
+  }
+
+
+  // For in loop
+
+  // While loop
+}
