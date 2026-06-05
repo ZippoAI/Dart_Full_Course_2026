164e39b56d662d2bf1486a847c51a87db0ef69f5
ZippoAI <iamrealzippo@gmail.com>
2026-06-06 00:34:55 +0530

    Dart Type conversion done

---
+++ 10.DartStringToInteger.dart
@@ -0,0 +1,29 @@
+void main() {
+  // String to int
+  var a, b, c;
+  a = 20;
+  b = "1";
+
+  c = a + int.parse(b);
+
+  print('$a + $b = $c');
+
+  // String to double
+
+  var d, e, f;
+  d = 40;
+  e = "0.1";
+  f = d + double.parse(e);
+
+  print('$d + $e = $f');
+
+  // int to string
+
+  var g, h, i;
+
+  g = 40;
+  h = '1';
+  i = g.toString() + h;
+  
+  print('$g + $h = $i');
+}
