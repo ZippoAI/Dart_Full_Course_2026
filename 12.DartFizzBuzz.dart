632593dcf5c907de14edbb27a96e126630dd42e4
ZippoAI <iamrealzippo@gmail.com>
2026-06-07 23:42:03 +0530

    fizz buzz practice done

---
+++ 12.DartFizzBuzz.dart
@@ -0,0 +1,15 @@
+void main() {
+  var num = 1;
+
+  for (var i = num; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      print('FizzBuzz');
+    } else if (i % 3 == 0) {
+      print('Fizz');
+    } else if (i % 5 == 0) {
+      print('Buzz');
+    } else {
+      print(i);
+    }
+  }
+}
