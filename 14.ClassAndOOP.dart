e3854d6f965d103e7ab2115267c61d2a78786540
ZippoAI <iamrealzippo@gmail.com>
2026-06-10 22:34:16 +0530

    dart

--- 14.ClassAndOOP.dart
+++ 14.ClassAndOOP.dart
@@ -1,11 +1,23 @@
-void main(){
+void main() {
+  Person p1 = Person('Zippo', 'Male', 22);
 
-
-
-
-
+  p1.showData();
   
+}
 
+class Person {
+  String? name, sex;
+  int? age;
 
+  Person(String name, sex, int age) {
+    this.name = name;
+    this.sex = sex;
+    this.age = age;
+  }
 
+  // Method
+
+  void showData() {
+    print('Name = $name\nSex = $sex\nAge = $age');
+  }
 }
